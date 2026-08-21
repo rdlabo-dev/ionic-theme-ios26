baba6cf7629a6392338bbb037723113be568a04f
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2026-08-22 02:47:26 +0900

    fix(list): support two-line content in inset items (#115)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- demo/src/app/settings/settings-page.component.ts
+++ demo/src/app/settings/settings-page.component.ts
@@ -10,7 +10,6 @@ import {
   IonFooter,
   IonHeader,
   IonIcon,
-  IonImg,
   IonItem,
   IonItemGroup,
   IonLabel,
@@ -21,6 +20,7 @@ import {
   IonToggle,
   IonToolbar,
   Platform,
+  IonListHeader,
 } from '@demo/ionic';
 
 @Component({
@@ -43,10 +43,10 @@ import {
     IonSearchbar,
     IonBackButton,
     IonAvatar,
-    IonImg,
     IonNote,
     IonItemGroup,
     IonToggle,
+    IonListHeader,
   ],
 })
 export class SettingsPage {
