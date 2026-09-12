8476c879dca8481a99b0b4c5be05d2c89624753a
rdlabo <sakakibara@rdlabo.jp>
2026-09-12 14:15:33 +0900

    fix: align tab bar interactions with iOS 27

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,18 @@ export * from './tab-bar-searchable';
 export * from './transition/ios.transition';
 
 export const registerTabBarEffect = (targetElement: HTMLElement): registeredEffect | undefined => {
-  return registerEffect(targetElement, 'ion-tab-button', 'tab-selected', {
-    small: 'scale(1.1, 1)',
-    medium: 'scale(1.2)',
-    large: 'scale(1.3)',
-    xlarge: 'scale(1.15, 1.4)',
-  });
+  return registerEffect(
+    targetElement,
+    'ion-tab-button',
+    'tab-selected',
+    {
+      small: 'scale(1.1, 1)',
+      medium: 'scale(1.2)',
+      large: 'scale(1.15, 1.4)',
+      xlarge: 'scale(1.3)',
+    },
+    1.2,
+  );
 };
 
 export const registerSegmentEffect = (targetElement: HTMLElement): registeredEffect | undefined => {
